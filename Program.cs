fa66940023465c33bf66c215fd1be943e7b84936
Armando Aranda <arandaae19@gmail.com>
2022-05-04 13:54:20 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(65);
+            var fahrenheit = TempConverter.CelsiusToFahreheit(20);
+
+            Console.WriteLine($"Converting 65 degrees Fahrenheit to Celsius: {celsius}");
+            Console.WriteLine();
+            Console.WriteLine($"Converting 20 degrees Celsius to Fahrenheit: {fahrenheit}");
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) / 1.8;
+
+        }
+
+        public static double CelsiusToFahreheit(double celsius)
+        {
+            return (celsius * 1.8) + 32;
+        }
+
+    }
+}
